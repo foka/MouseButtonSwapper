0e865abf1ed16ece89799dad8a025b7371b08ad2
foka <michal.foka.wlodarczyk@gmail.com>
2012-02-17 20:09:59 +0100

    Notify icon with "delayed" context menu; swap function in menu and on notify icon double click (any button hence delayed context menu)

---
+++ MouseButtonsSwapper/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MouseButtonsSwapper
+{
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			using (new SwapperView(new Swapper()))
+				Application.Run();
+		}
+	}
+}
---
+++ MouseButtonsSwapper/SwapperView.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace MouseButtonsSwapper
+{
+	internal class SwapperView : IDisposable
+	{
+		public SwapperView(Swapper swapper)
+		{
+			this.swapper = swapper;
+			notifyIcon = new NotifyIcon
+			{
+				Icon = GetCurrentIcon(),
+				Text = Resources.IconTooltip,
+				Visible = true,
+			};
+			notifyIcon.MouseClick += notifyIcon_Click;
+			notifyIcon.MouseDoubleClick += (sender, args) => { SwapButtons(); notifyIconClickTimer.Enabled = false; };
+
+			contextMenu = CreateContextMenu();
+
+			notifyIconClickTimer = new Timer { Interval = SystemInformation.DoubleClickTime };
+			notifyIconClickTimer.Tick += notifyIconClickTimer_Tick;
+		}
+
+
+		private void notifyIconClickTimer_Tick(object sender, EventArgs e)
+		{
+			notifyIconClickTimer.Enabled = false;
+
+			notifyIcon.ContextMenu = contextMenu;
+			var mi = typeof(NotifyIcon).GetMethod("ShowContextMenu", BindingFlags.Instance | BindingFlags.NonPublic);
+			mi.Invoke(notifyIcon, null);
+			notifyIcon.ContextMenu = null;
+		}
+
+
+		private void notifyIcon_Click(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+				return;
+
+			// second button clicked:
+
+			if (notifyIconClickTimer.Enabled)
+			{
+				notifyIconClickTimer.Enabled = false;
+				SwapButtons();
+			}
+			else
+			{
+				notifyIconClickTimer.Enabled = true;
+			}
+		}
+
+
+		private ContextMenu CreateContextMenu()
+		{
+			var menu = new ContextMenu();
+
+			menu.MenuItems.Add(Resources.MenuSwap, (s, a) => SwapButtons()).Enabled = false;
+			menu.MenuItems.Add("-");
+//			TODO:
+//			menu.MenuItems.Add(Resources.ChangeHotkey);
+//			menu.MenuItems.Add(Resources.RunOnLogin, (s, a) => ((MenuItem)s).Checked = !((MenuItem)s).Checked)
+//				.Checked = true;
+//			menu.MenuItems.Add("-");
+			menu.MenuItems.Add(Resources.MenuExit, (s, a) => Application.Exit());
+
+			return menu;
+		}
+
+		private Icon GetCurrentIcon()
+		{
+			return swapper.IsSwapped ? Resources.MouseSwapped : Resources.MouseNormal;
+		}
+
+		private void SwapButtons()
+		{
+			swapper.SwapButtons();
+			notifyIcon.Icon = GetCurrentIcon();
+		}
+
+
+		public void Dispose()
+		{
+			notifyIcon.Dispose();
+		}
+
+
+		private readonly Swapper swapper;
+		private readonly NotifyIcon notifyIcon;
+		private readonly Timer notifyIconClickTimer;
+		private readonly ContextMenu contextMenu;
+	}
+}
