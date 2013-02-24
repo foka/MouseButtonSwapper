8dc8b4b1939d554c664485478d68383c7ca537f1
foka <michal.foka.wlodarczyk@gmail.com>
2013-02-24 13:23:25 +0100

    Swap state is now remembered after relogin
    fixes #1

--- MouseButtonSwapper/Swapper.cs
+++ MouseButtonSwapper/Swapper.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
-
+using Microsoft.Win32;
 
 namespace MouseButtonSwapper
 {
@@ -12,12 +12,12 @@ namespace MouseButtonSwapper
 			get { return SystemInformation.MouseButtonsSwapped; }
 		}
 
-
 		public void SwapButtons()
 		{
 			SwapMouseButton(IsSwapped ? 0 : 1);
+			using (var swapKey = Registry.CurrentUser.CreateSubKey(@"Control Panel\Mouse"))
+				swapKey.SetValue("SwapMouseButtons", IsSwapped ? "1" : "0", RegistryValueKind.String);
 		}
-
 
 		[DllImport("user32.dll")]
 		private static extern Int32 SwapMouseButton(int bSwap);
